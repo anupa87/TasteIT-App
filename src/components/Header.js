0e466789f2b55bdaa650e3af84c492a7a7ecc5fa
Thapa Anupa <s2200134@BC-C02GR0C2Q6LR-MacBook-Air.local>
2022-05-09 13:27:28 +0300

    initial clean up

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-top">
+        <div className="logo">
+        </div>
+        <div className="navbar">
+        </div>
+      </div>
+
+      <div className="header-bottom">
+        <div className="intro-video"></div>
+        <div className="browse-btn"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
